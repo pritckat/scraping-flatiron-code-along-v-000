603a22d53c5770b32d28f809a38be6c865ec9bad
Katrina Pritchard <pritckat@gmail.com>
2018-08-22 21:33:23 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -16,12 +16,12 @@ end
 
 Scraper.new.get_page
 
-def print_courses
-self.make_courses
-Course.all.each do |course|
-  if course.title
-    puts "Title: #{course.title}"
-    puts "  Schedule: #{course.schedule}"
-    puts "  Description: #{course.description}"
-  end
-end
+#def print_courses
+#self.make_courses
+#Course.all.each do |course|
+#  if course.title
+#    puts "Title: #{course.title}"
+#    puts "  Schedule: #{course.schedule}"
+#    puts "  Description: #{course.description}"
+#  end
+#end
