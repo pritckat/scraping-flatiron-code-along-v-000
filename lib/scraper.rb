72a09e54c979c740ce2d224c3a72d11dff97488b
Katrina Pritchard <pritckat@gmail.com>
2018-08-22 21:31:09 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,10 +1,11 @@
 require 'nokogiri'
 require 'open-uri'
+require 'pry'
 
 require_relative './course.rb'
 
 class Scraper
-  
+
     def print_courses
     self.make_courses
     Course.all.each do |course|
@@ -15,8 +16,8 @@ class Scraper
       end
     end
   end
-  
+
+  def get_page
+
+  end
 end
-
-
-
